8b5d9c8de14f5f1d79f391203af8b38a2b9cf740
AbdurahmanIdris <abduidrism@gmail.com>
2024-01-29 09:41:43 +0300

    provider in stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final nameProvider = Provider<String>(
+  (ref) {
+    return "Abdurahman Idris";
+  },
+);
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,12 +19,49 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        
+      /*theme: ThemeData(        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+      ),*/
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          color: Colors.deepPurple,
+          titleTextStyle:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
       ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    //method 1
+    // final name = ref.watch(nameProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Riverpod Provider"),
+      ),
+      //method 1
+      /*body: Center(
+        child: Text("Hello " + name + "!"),
+      ),*/
+
+      //method 2
+      body: Center(
+        child: Consumer(
+          builder: (context, ref, child) {
+            final name = ref.watch(nameProvider);
+            return Text(name);
+          },
+        ),
+      ),
     );
   }
 }
