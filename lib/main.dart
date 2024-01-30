ea4bb295d94b394ba8f81885c24634e83ef447ff
AbdurahmanIdris <abduidrism@gmail.com>
2024-01-30 10:08:57 +0300

    Theme Switcher added

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod_new/counter.dart';
-import 'package:flutter_riverpod_new/home.dart';
-import 'package:go_router/go_router.dart';
+
+final themeProvider = StateProvider<bool>((ref) => true);
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter Demo',
-      /*theme: ThemeData(        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),*/
-      theme: ThemeData(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLightTheme = ref.watch(themeProvider);
+
+    return MaterialApp(
+      title: 'Theme',
+      theme: isLightTheme ? ThemeData.light() : ThemeData.dark(),
+      /*theme: ThemeData(
         primarySwatch: Colors.blue,
         appBarTheme: const AppBarTheme(
           color: Colors.deepPurple,
           titleTextStyle:
               TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
-      ),
-      // home: const MyHomePage(),
-      routerConfig: _router,
+      ),*/
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-final GoRouter _router = GoRouter(routes: [
-  GoRoute(path: '/', builder: (context, state) => const Home()),
-  GoRoute(path: '/counter', builder: (context, state) => const Counter()),
-]);
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLightTheme = ref.watch(themeProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Theme"),
+      ),
+      body: Center(
+          child: Switch(
+        value: isLightTheme,
+        onChanged: (value) => ref.read(themeProvider.notifier).state = value,
+      )),
+    );
+  }
+}
